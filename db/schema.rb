c4d88d5fd0a7bd94892ecdcca94c8e4bc0da7159
shinhee kim <ariel720@naver.com>
2016-12-09 02:32:33 +0000

    create users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209000520) do
+ActiveRecord::Schema.define(version: 20161209022606) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20161209000520) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
   end
 
 end
