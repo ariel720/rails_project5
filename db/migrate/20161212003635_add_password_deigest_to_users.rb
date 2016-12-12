8a5118cee97a9611021ba7fc3dcfd07e47ff6364
shinhee kim <ariel720@naver.com>
2016-12-12 01:23:04 +0000

    create user signup

---
+++ db/migrate/20161212003635_add_password_deigest_to_users.rb
@@ -0,0 +1,6 @@
+class AddPasswordDeigestToUsers < ActiveRecord::Migration
+  def change
+    
+    add_column :users, :password_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209030845) do
+ActiveRecord::Schema.define(version: 20161212003635) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20161209030845) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
